04f2b7e72562281b95bcfdd7c40b8a3f24505e0e
Luídi <luidihot@gmail.com>
2022-09-09 19:18:32 -0300

    Requisitos 05 - 11

---
+++ src/tests/Feedback.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouterAndRedux from './renderWithRouterAndRedux';
+import Feedback from '../pages/Feedback'
+
+
+describe('testa a pagina de Feedback', () => {
+it('', () => {
+    
+})
+
+it('Testa a URL', () => {
+    const{ history, }
+})
+
+it('', () => {
+    
+})
+
+});
